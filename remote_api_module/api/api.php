093a7f17d3f15f67f8f2ebac499d34b5bd175581
Dody <dodyrw@gmail.com>
2011-09-13 09:58:10 +0700

    Update remote API, fix quote problem (only special case, different php setting)

--- remote_api_module/api/api.php
+++ remote_api_module/api/api.php
@@ -14,18 +14,25 @@ include '../global.php';
 include 'json.php';
 
 // turn on/off debug mode
-// $lep->db->debug=true;;
+$lep->db->debug=true;
 
 ///////////////////////////////////////////////////////////////////
 
-$raw_data = $_POST['data'];
+$raw_data = $_POST['data']; 
+
+// use stripslashes if json_unserialize return no value
+// $raw_data = stripslashes($raw_data);
+
 $data = json_unserialize($raw_data);
+
+// debug data value
+// print_r($raw_data);
+// print_r($data);
 
 switch ($_POST['act']) {
 
   // +++++ Add listing +++++
   case 'res_add':
-
     if (is_array($data)) {
       foreach ($data as $k => $v) {
         $record = $v;
@@ -49,7 +56,7 @@ switch ($_POST['act']) {
           $insert_id = $record['res_id'];
         }
 
-        seo_build_seo_title('resource', $insert_id);
+        build_seo_title2($insert_id);
         print ".";
       }
     }
@@ -67,7 +74,7 @@ switch ($_POST['act']) {
 
         $lep->db->AutoExecute('lep_resource', $record, 'UPDATE',
           "res_id = '{$record['res_id']}'");
-        seo_build_seo_title('resource', $record['res_id']);
+        build_seo_title2($record['res_id']);
         
         print ".";
       }
@@ -78,5 +85,27 @@ switch ($_POST['act']) {
   default:
     # code...
     break;
+}
+
+
+function build_seo_title2($id) {
+  global $lep;
+  
+  $query = "select title from lep_resource where res_id = '$id'";
+  $title = $lep->db->GetOne($query);
+  $seo_title = seo_title($title);
+
+  $title = addslashes($title);
+  
+  // check duplicate seo title
+  $query = "select count(res_id) from lep_resource where title = '{$title}' and res_id <> '{$id}'";
+  $num_dup = $lep->db->GetOne($query);
+  
+  if ($num_dup) {
+    $seo_title = seo_title($title).'-'.$num_dup;
+  }
+  
+  $query = "update lep_resource set seo_title = '$seo_title' where res_id = '$id'";
+  $rs = $lep->db->Execute($query);
 }
 ?>
